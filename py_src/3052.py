ec0d7dbf6f2792d85ba009990dc7db8e0a4a2be4
unexistentman <chan3358@naver.com>
2021-04-17 19:29:45 +0900

    step5

--- py_src/3052.py
+++ py_src/3052.py
@@ -1,13 +1,15 @@
 a = list()
-c = 0
+#c = 0
 
 for i in range(10):
     b = int(input())
     a.append(b%42)
 
+a_set = set(a)
+'''
 for i in range(42):
     if i in a:
         c += 1
-
-print(c)
+'''
+print(len(a_set))
 
