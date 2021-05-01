058930e10c5c207380ebcb5219f3eb9c6e757271
unexistentman <chan3358@naver.com>
2021-05-01 16:07:24 +0900

    update

---
+++ py_src/step7/1316.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    word=input()
+
+    for j in range(len(word)-1):
+        if word[j] != word[j+1]:
+            if word[j] in word[j+1:]:
+                n -= 1
+                break
+print(n)
