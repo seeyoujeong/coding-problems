4dde523e9d91b1b6d900d3e80d16e860e98bfdaf
unexistentman <chan3358@naver.com>
2021-05-26 16:15:31 +0900

    add

---
+++ py_src/step12/11651.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+array = list()
+
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    array.append((x,y))
+
+array.sort()   
+array.sort(key = lambda x : x[1])
+
+for i in array:
+    print(*i)
