2a2e920ba58617296529656c3ab93d8d7ebd6d0b
unexistentman <chan3358@naver.com>
2021-05-30 22:13:04 +0900

    add

---
+++ py_src/step14/15649.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+s = list()
+
+def f():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(1, n + 1):
+        if i in s:
+            continue
+        s.append(i)
+        f()
+        s.pop()
+
+f()
+
