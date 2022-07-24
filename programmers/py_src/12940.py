4246bfe7548e935df5f6ed1dc6aa64fa65017207
unexistentman <chan3358@naver.com>
2022-07-24 13:11:27 +0900

    add

---
+++ programmers/py_src/12940.py
@@ -0,0 +1,16 @@
+# 최대공약수와 최소공배수
+
+def solution(n, m):
+    answer = []
+
+    def gcd(n, m):
+        r = n % m
+        if r == 0:
+            return m
+        else:
+            return gcd(m, r)
+
+    answer.append(gcd(n, m))
+    answer.append(n * m / gcd(n, m))
+
+    return answer
---
+++ programmers/py_src/12944.py
@@ -0,0 +1,6 @@
+# 평균 구하기
+
+def solution(arr):
+    answer = sum(arr) / len(arr)
+
+    return answer
