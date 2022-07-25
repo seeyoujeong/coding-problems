7f092bfbb6994311ec4f9f125dda979fd6716ff7
unexistentman <chan3358@naver.com>
2022-07-25 23:41:02 +0900

    add

---
+++ programmers/py_src/12932.py
@@ -0,0 +1,14 @@
+# 자연수 뒤집어 배열로 만들기
+
+def solution(n):
+    answer = []
+    strNum = str(n)
+    
+    for i in strNum:
+        answer.append(int(i))
+        
+    answer.reverse()
+    
+    return answer
+    
+    # return list(map(int, reversed(str(n))))
