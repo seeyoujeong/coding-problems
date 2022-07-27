997c1d0b5fb65f076aee1d0a6c2bff0120d155f2
unexistentman <chan3358@naver.com>
2022-07-27 14:10:05 +0900

    add

---
+++ programmers/py_src/12947.py
@@ -0,0 +1,16 @@
+# 하샤드 수
+
+def solution(x):
+    answer = True
+    numList = list(map(int, str(x)))
+
+    if x % sum(numList) == 0:
+        answer = True
+    else:
+        answer = False
+
+    return answer
+  
+# 다른 풀이
+# def Harshad(n):
+#     return n % sum([int(c) for c in str(n)]) == 0
