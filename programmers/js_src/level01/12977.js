693887918ba63ced909f6069b1217ab258290a25
unexistentman <chan3358@naver.com>
2022-08-03 13:37:30 +0900

    add

---
+++ programmers/js_src/level01/12977.js
@@ -0,0 +1,30 @@
+// 소수 만들기
+
+function solution(nums) {
+  var answer = 0;
+
+  for (let i = 0; i + 2 < nums.length; i++) {
+    for (let j = i + 1; j + 1 < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
+function isPrime(num) {
+  if (num == 1) {
+    return false;
+  } else {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
