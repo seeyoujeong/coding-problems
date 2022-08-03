071d5f04e1274849c80a93c423b54e34547d6ba0
unexistentman <chan3358@naver.com>
2022-08-03 22:28:43 +0900

    add

---
+++ programmers/js_src/level01/64061.js
@@ -0,0 +1,26 @@
+// 크레인 인형뽑기 게임
+
+function solution(board, moves) {
+  var answer = 0;
+  let arr = [];
+
+  for (let col of moves) {
+    for (let row of board) {
+      if (row[col - 1] != 0) {
+        arr.push(row[col - 1]);
+        row[col - 1] = 0;
+        break;
+      }
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == arr[i - 1]) {
+      answer += 2;
+      arr.splice(i - 1, 2);
+      i = 0;
+    } 
+  }
+
+  return answer;
+}
