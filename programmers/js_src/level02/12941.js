39055629806879d3e488d0f385c0841c441df419
unexistentman <chan3358@naver.com>
2022-08-04 13:36:08 +0900

    add

---
+++ programmers/js_src/level02/12941.js
@@ -0,0 +1,22 @@
+// 최솟값 만들기
+
+function solution(A,B){
+  var answer = 0;
+
+  // 비효율
+  // while (A.length) {
+  //     answer += Math.max(...A) * Math.min(...B);
+
+  //     A.splice(A.indexOf(Math.max(...A)), 1);
+  //     B.splice(B.indexOf(Math.min(...B)), 1);
+  // }
+
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[i];
+  }
+
+  return answer;
+}
