112fb9d79e16b61832395c5dd70eb7cc11f30409
unexistentman <chan3358@naver.com>
2022-08-09 14:26:53 +0900

    add

---
+++ programmers/js_src/level02/27701.js
@@ -0,0 +1,33 @@
+// N개의 최소공배수
+
+function solution(arr) {
+  var answer = 0;
+  
+  answer = arr.reduce((a, b) => lcm(a, b), 1);
+  
+  return answer;
+}
+
+function gcd(n, m) {
+  let r = m % n;
+  
+  if (r == 0) {
+    return n;
+  } else {
+    return gcd(r, n);
+  }
+
+  // while (m > 0) {
+  //   let r = n % m;
+  //   n = m;
+  //   m = r;
+  // }
+
+  // return n;
+}
+
+function lcm(n, m) {
+  return n * m / gcd(n, m);
+}
+
+console.log(2 % 4);
