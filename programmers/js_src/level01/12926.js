32f3cc1e56fc140807ad6fed8baa20b8fdc9a4b7
unexistentman <chan3358@naver.com>
2022-08-09 16:48:45 +0900

    modify

--- programmers/js_src/level01/12926.js
+++ programmers/js_src/level01/12926.js
@@ -8,13 +8,13 @@ function solution(s, n) {
       if (j.codePointAt(0) + n > 'z'.codePointAt(0)) {
         return String.fromCodePoint(
           'a'.codePointAt(0) + j.codePointAt(0) + n - 'z'.codePointAt(0) - 1);
-      } else if (j.codePointAt(0) <= 'Z'.codePointAt(0) && j.codePointAt(0) + n > 'Z'.codePointAt(0)) {
+      } else if (j.codePointAt(0) <= 'Z'.codePointAt(0) && 
+                  j.codePointAt(0) + n > 'Z'.codePointAt(0)) {
         return String.fromCodePoint(
           'A'.codePointAt(0) + j.codePointAt(0) + n - 'Z'.codePointAt(0) - 1);
       }
-
-      return String.fromCodePoint(j.codePointAt(0) + n)
-    }).join('')
+      return String.fromCodePoint(j.codePointAt(0) + n);
+    }).join('');
   }).join(' ');
 
   return answer;
--- programmers/js_src/level01/1845.js
+++ programmers/js_src/level01/1845.js
@@ -18,5 +18,5 @@ function solution(nums) {
 //   const max = nums.length / 2;
 //   const arr = [...new Set(nums)];
 
-//   return arr.length > max ? max : arr.length
+//   return arr.length > max ? max : arr.length;
 // }
