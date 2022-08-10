c767c5dcc4c662d10458025cf224f92bdeaaeff0
unexistentman <chan3358@naver.com>
2022-08-11 00:03:14 +0900

    add

---
+++ programmers/js_src/level02/70129.js
@@ -0,0 +1,30 @@
+// 이진 변환 반복하기
+
+function solution(s) {
+  var answer = [0, 0];
+
+  while (s != 1) {
+    let len = s.length;
+    let removelen = s.split('0').join('').length;
+
+    s = removelen.toString(2);
+
+    answer[0]++;
+    answer[1] += len - removelen;
+  }
+
+  return answer;
+}
+
+// 다른 풀이
+// function solution(s) {
+//   var answer = [0, 0];
+
+//   while(s.length > 1) {
+//     answer[0]++;
+//     answer[1] += (s.match(/0/g) || []).length;
+//     s = s.replace(/0/g, '').length.toString(2);
+//   }
+
+//   return answer;
+// }
