06409ab9a95ccd1e2eb75d2d6ca97d7614024503
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-10-07 20:23:51 +0900

    add

---
+++ programmers/js_src/level00/120802.js
@@ -0,0 +1,8 @@
+// 두 수의 합
+function solution(num1, num2) {
+  var answer = -1;
+
+  answer = num1 + num2;
+
+  return answer;
+}
---
+++ programmers/js_src/level00/120805.js
@@ -0,0 +1,8 @@
+// 몫 구하기
+function solution(num1, num2) {
+  var answer = 0;
+
+  answer = Math.trunc(num1 / num2);
+
+  return answer;
+}
