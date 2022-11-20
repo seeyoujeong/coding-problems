18fca63d87be32004266d56b94a4806b03ced5d7
jeong <chan3358@naver.com>
2022-11-20 09:58:14 +0900

    add

---
+++ baekjoon/js/10171.js
@@ -0,0 +1,5 @@
+// 고양이
+console.log('\\    /\\');
+console.log(' )  ( \')');
+console.log('(  /  )');
+console.log(' \\(__)|');
---
+++ baekjoon/js/10172.js
@@ -0,0 +1,6 @@
+// 개
+console.log('|\\_/|');
+console.log('|q p|   /}');
+console.log('( 0 )"""\\');
+console.log('|"^"`    |');
+console.log('||_/=\\\\__|');
