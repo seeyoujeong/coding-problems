522a57bbe2281a3b17ed4c620fffed59c8eecac9
jeong <chan3358@naver.com>
2022-12-23 22:41:21 +0900

    modify

--- programmers/js/level00/120826.js
+++ programmers/js/level00/120826.js
@@ -2,3 +2,12 @@
 function solution(my_string, letter) {
   return my_string.split(letter).join('');
 }
+
+// 다른 풀이
+// function solution(my_string, letter) {
+//   return my_string.replace(new RegExp(`${letter}`, 'g'), '');
+// }
+
+// function solution(my_string, letter) {
+//   return my_string.replaceAll(letter, '');
+// }
