5ac68df9654b17e821b21b3af9c9d928ce6e5f09
jeong <chan3358@naver.com>
2023-02-17 11:52:17 +0900

    add

---
+++ programmers/js/level01/159994.js
@@ -0,0 +1,14 @@
+// 카드 뭉치
+function solution(cards1, cards2, goal) {
+  for (let card of goal) {
+    if (cards1[0] === card) {
+      cards1.shift();
+    } else if (cards2[0] === card) {
+      cards2.shift();
+    } else {
+      return 'No';
+    }
+  }
+  
+  return 'Yes';
+}
