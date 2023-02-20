4d615614915f7f53833d572c7c760082707af726
jeong <chan3358@naver.com>
2023-02-20 15:31:28 +0900

    modify

--- programmers/js/level01/42576.js
+++ programmers/js/level01/42576.js
@@ -1,34 +1,14 @@
 // 완주하지 못한 선수
 function solution(participant, completion) {
-  var answer = '';
-  
   participant.sort();
   completion.sort();
   
   for (let i = 0; i < participant.length; i++) {
-    if (participant[i] != completion[i]) {
-      answer = participant[i];
-      break;
+    if (participant[i] !== completion[i]) {
+      return participant[i];
     }
   }
-  
-  return answer;
 }
-
-// 효율↓
-// function solution(participant, completion) {
-//   var answer = '';
-
-//   completion.forEach(c => {
-//     if (participant.includes(c)) {
-//       participant.splice(participant.indexOf(c), 1);
-//     }
-//   });
-  
-//   answer = participant[0];
-  
-//   return answer;
-// }
 
 // 다른 풀이
 // function solution(participant, completion) {
@@ -40,16 +20,21 @@ function solution(participant, completion) {
 //   const map = new Map();
 
 //   for(let i = 0; i < participant.length; i++) {
-//     let a = participant[i];
-//     let b = completion[i];
+//     let pName = participant[i];
+//     let cName = completion[i];
 
-//     map.set(a, (map.get(a) || 0) + 1);
-//     map.set(b, (map.get(b) || 0) - 1);
+//     map.set(pName, (map.get(pName) || 0) + 1);
+//     map.set(cName, (map.get(cName) || 0) - 1);
 //   }
 
-//   for(let [k, v] of map) {
-//     if(v > 0) return k;
+//   for(let [name, num] of map) {
+//     if (num > 0) return name;
 //   }
+// }
 
-//   return 'nothing';
+// function solution(participant, completion) {
+//   participant.sort();
+//   completion.sort();
+    
+//   return participant.find((name, idx) => name !== completion[idx]);
 // }
--- programmers/js/level01/42748.js
+++ programmers/js/level01/42748.js
@@ -1,26 +1,8 @@
 // K번째수
 function solution(array, commands) {
-  var answer = [];
-
-  for (let command of commands) {
-    let arraySlice = array.slice(command[0] - 1, command[1]);
-
-    arraySlice.sort((a, b) => a - b);
-
-    answer.push(arraySlice[command[2] - 1]);
-  }
-
-  return answer;
+  return commands
+    .map(([start, end, ord]) => 
+      array
+        .slice(start - 1, end)
+        .sort((a, b) => a - b)[ord - 1]);
 }
-
-// 다른 풀이
-// function solution(array, commands) {
-//   return commands.map(command => {
-//     const [sPosition, ePosition, position] = command
-//     const newArray = array
-//       .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
-//       .sort((a,b) => a - b)    
-
-//     return newArray[position - 1]
-//   })
-// }
