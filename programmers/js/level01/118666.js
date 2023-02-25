e9c28ec6bff4bf605f8a73d76ff2da4c5e7190c9
jeong <chan3358@naver.com>
2023-02-25 14:05:01 +0900

    modify

--- programmers/js/level01/118666.js
+++ programmers/js/level01/118666.js
@@ -1,32 +1,19 @@
 // 성격 유형 검사하기
 function solution(survey, choices) {
   let answer = '';
-  const category = {
-      R: 0,
-      T: 0,
-      C: 0,
-      F: 0,
-      J: 0,
-      M: 0,
-      A: 0,
-      N: 0,
-  };
+  const types = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'];
+  const typesScore = types.reduce((acc, cur) => ({...acc, [cur]: 0}), {});
 
   for (let i = 0; i < choices.length; i++) {
-    if (choices[i] < 4) {
-      category[survey[i][0]] += 4 - choices[i];
-    }
-
-    if (choices[i] > 4) {
-      category[survey[i][1]] += choices[i] - 4;
-    }
+    const [type1, type2] = survey[i];  
+      
+    typesScore[choices[i] < 4 ? type1 : type2] += Math.abs(choices[i] - 4);
   }
-    
-  const categoryKeys = Object.keys(category);
 
-  for (let i = 1; i < categoryKeys.length; i += 2) {
-    const [key1, key2] = [categoryKeys[i - 1], categoryKeys[i]];
-    answer += category[key1] >= category[key2] ? key1 : key2;
+  for (let i = 1; i < types.length; i += 2) {
+    const [type1, type2] = [types[i - 1], types[i]];
+      
+    answer += typesScore[type1] >= typesScore[type2] ? type1 : type2;
   }
 
   return answer;
@@ -34,23 +21,18 @@ function solution(survey, choices) {
 
 // 다른 풀이
 // function solution(survey, choices) {
-//   var answer = '';
-//   let indi = new Map();
-//   ['R','T', 'C','F', 'J','M', 'A', 'N'].forEach(item => indi.set(item, 0));
+//   const MBTI = {};
+//   const types = ["RT","CF","JM","AN"];
 
-//   choices.forEach((item,index) => {
-//     let [A, B] = survey[index].split('');
-//     if(item > 4) {
-//       indi.set(B, indi.get(B) + item - 4);
-//     } else if (item < 4) {
-//       indi.set(A, indi.get(A) + 4 - item);
-//     } 
+//   types.forEach((type) =>
+//     type.split('').forEach((char) => MBTI[char] = 0)
+//   );
+
+//   choices.forEach((choice, index) => {
+//     const [disagree, agree] = survey[index];
+
+//     MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
 //   });
 
-//   answer += indi.get('R') >= indi.get('T') ? 'R' : 'T';
-//   answer += indi.get('C') >= indi.get('F') ? 'C' : 'F';
-//   answer += indi.get('J') >= indi.get('M') ? 'J' : 'M';
-//   answer += indi.get('A') >= indi.get('N') ? 'A' : 'N';
-
-//   return answer;
+//   return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
 // }
