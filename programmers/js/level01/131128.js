d4636e19bfc08ff2544d40878f6e86ac8685b965
jeong <chan3358@naver.com>
2023-02-26 10:55:14 +0900

    modify

--- programmers/js/level01/131128.js
+++ programmers/js/level01/131128.js
@@ -1,11 +1,10 @@
 // 숫자 짝꿍
 function solution(X, Y) {
-  var answer = '';
-  const arrX = X.split('');
-  const arrY = Y.split('');
-  const setX = new Set(X);
+  let answer = '';
+  const arrX = [...X];
+  const arrY = [...Y];
 
-  for (let x of setX) {
+  for (let x of new Set(X)) {
     if (Y.includes(x)) {
       const lenX = arrX.filter(v => x === v).length;
       const lenY = arrY.filter(v => x === v).length;
@@ -22,7 +21,40 @@ function solution(X, Y) {
     return '0';
   }
 
-  answer = answer.split('').sort((a, b) => b - a).join('');
-
-  return answer;
+  return [...answer].sort((a, b) => b - a).join('');
 }
+
+// 다른 풀이
+// function solution(X, Y) {
+//   const [arrX, arrY] = [[...X], [...Y]];
+//   const answer = [...new Set(X)].reduce((acc, cur) => acc + (Y.includes(cur) ? 
+//           cur.repeat(Math.min(arrX.filter(v => cur === v).length, 
+//                               arrY.filter(v => cur === v).length)) : 
+//           ''), '');
+  
+//   return answer.length ? 
+//       (+answer ? [...answer].sort((a, b) => b - a).join('') : '0') :
+//       '-1';
+// }
+
+// function solution(X, Y) {
+//   let result = '';
+//   const numObj = {};
+
+//   for (const char of X) {
+//     numObj[char] = (numObj[char] | 0) + 1;
+//   }
+
+//   for (const char of Y) {
+//     if (!numObj[char]) continue;
+//     result += char;
+//     numObj[char]--;
+//   }
+
+//   if (result === '') return '-1';
+//   if (+result === 0) return '0';
+//   return [...result]
+//     .map(Number)
+//     .sort((a, b) => b - a)
+//     .join('');    
+// }
--- programmers/js/level01/132267.js
+++ programmers/js/level01/132267.js
@@ -1,13 +1,18 @@
 // 콜라 문제
 function solution(a, b, n) {
-  var answer = 0;
+  let answer = 0;
   let bottle = n;
 
   while (bottle >= a) {
-    const tmp = parseInt(bottle / a) * b;
-    bottle = tmp + parseInt(bottle % a);
+    const tmp = ~~(bottle / a) * b;
+    bottle = tmp + (bottle % a);
     answer += tmp;
   }
 
   return answer;
 }
+
+// 다른 풀이
+// function solution(a, b, n) {
+//   return ~~((n - b) / (a - b)) * b;
+// }
