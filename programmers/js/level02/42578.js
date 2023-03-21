3092c0d7cc20c821fa5595f595e207302f92980a
jeong <chan3358@naver.com>
2023-03-21 11:19:26 +0900

    modify

--- programmers/js/level02/42578.js
+++ programmers/js/level02/42578.js
@@ -1,53 +1,32 @@
 // 위장
 function solution(clothes) {
-  var answer = 1; 
-  const clothesTypeAmount = {};
+  const clothesTypeAmount = clothes.reduce((acc, [_, type]) => {
+    acc[type] = acc[type] + 1 || 1;
+    return acc;
+  }, {});
 
-  clothes.forEach(([name, type]) => {
-    clothesTypeAmount[type] = 
-        type in clothesTypeAmount ? clothesTypeAmount[type] + 1 : 1;
-  });
-
-  Object.values(clothesTypeAmount).forEach(amount => {
-    answer *= 1 + amount;
-  })
-
-
-  return answer - 1;
+  return Object.values(clothesTypeAmount).reduce((acc, cur) => {
+    acc *= cur + 1;
+    return acc;
+  }, 1) - 1;
 }
-
-// 효율↓
-// function solution(clothes) {
-//   var answer = 0; 
-
-//   for (let i = 1; i <= clothes.length; i++) {
-//     if (getCombinations(clothes, i).length === 0) break;
-    
-//     answer += getCombinations(clothes, i).length;
-//   }
-  
-//   return answer;
-// }
-
-// function getCombinations(arr, num) {
-//   const results = [];
-  
-//   if (num === 1) return arr.map(elem => [elem]);
-  
-//   arr.forEach((fixed, index, origin) => {
-//     const rest = origin.slice(index + 1).filter(elem => fixed[1] !== elem[1]);
-//     const combinations = getCombinations(rest, num - 1);
-//     const attached = combinations.map(elem => [fixed, ...elem]);
-//     results.push(...attached);
-//   });
-  
-//   return results;
-// }
 
 // 다른 풀이
 // function solution(clothes) {
-//   return Object.values(clothes.reduce((obj, t)=> {
-//     obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
-//     return obj;
-//   } , {})).reduce((a,b)=> a * (b + 1), 1) - 1;    
+//   let answer = 1;
+//   const key = new Map();
+  
+//   for (const [_, type] of clothes) {
+//     if (key.has(type)) {
+//       key.set(type, key.get(type) + 1);
+//     } else {
+//       key.set(type, 1);
+//     }
+//   }
+  
+//   for (const val of key.values()){
+//     answer *= val + 1
+//   }
+
+//   return answer - 1;
 // }
