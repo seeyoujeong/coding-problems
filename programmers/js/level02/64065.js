39fe59488df4fb562b6e19839a72bc9c5d44c482
jeong <chan3358@naver.com>
2023-04-02 11:24:51 +0900

    refactor: modify

--- programmers/js/level02/64065.js
+++ programmers/js/level02/64065.js
@@ -1,22 +1,12 @@
 // 튜플
 function solution(s) {
-  var answer = [];
-  let array = [];
-  let set = new Set();
+  const arr = JSON.parse(
+    s.replaceAll(/[{}]/g, (match) => (match === "{" ? "[" : "]"))
+  )
+    .sort((a, b) => a.length - b.length)
+    .flat();
 
-  s = s.match(/{([^{^}]+)}/g);
-
-  s.forEach(elem => array.push(elem.match(/([0-9]+)/g)));
-
-  array.sort((a, b) => a.length - b.length);
-
-  array.forEach(arr => {
-    arr.forEach(elem => set.add(+elem));
-  });
-
-  answer = [...set];
-
-  return answer;
+  return [...new Set(arr)];
 }
 
 // 다른 풀이
--- programmers/js/level02/87390.js
+++ programmers/js/level02/87390.js
@@ -1,32 +1,7 @@
 // n^2 배열 자르기
 function solution(n, left, right) {
-    var answer = [];
-
-    for (let i = left; i <= right; i++) {
-      answer.push(Math.max(Math.trunc(i / n), i % n) + 1);
-    }
-
-    return answer;
+  return Array(right - left + 1)
+    .fill(0)
+    .map((_, idx) => left + idx)
+    .map((num) => Math.max(Math.floor(num / n), num % n) + 1);
 }
-
-// 효율↓
-// function solution(n, left, right) {
-//   var answer = [];
-//   let arr = new Array(n);
-  
-//   for (let i = 0; i < n; i++) {
-//     arr[i] = new Array(n);
-//   }
-  
-//   for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < n; j++) {
-//       if (j <= i) arr[i][j] = i + 1;
-//       if (j > i) arr[i][j] = j + 1;
-//     }
-//   }
-  
-//   answer = arr.flat().slice(left, right + 1);
-  
-//   return answer;
-// }
-
