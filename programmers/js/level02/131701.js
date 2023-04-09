266b645c4f636302af5ed7d3f1d85a05ddb30db2
jeong <chan3358@naver.com>
2023-04-09 22:30:44 +0900

    refactor: simplify 131701, add another solution

--- programmers/js/level02/131701.js
+++ programmers/js/level02/131701.js
@@ -1,18 +1,31 @@
 // 연속 부분 수열 합의 개수
 function solution(elements) {
-  var answer = 0;
   const len = elements.length;
   const result = new Set();
-  
+
   elements.push(...elements);
-  
-  for (let i = 1; i <= len; i++) {       
+
+  for (let i = 1; i <= len; i++) {
     for (let j = 0; j < len; j++) {
       result.add(elements.slice(j, i + j).reduce((acc, cur) => acc + cur, 0));
     }
   }
-  
-  answer = result.size;
-  
-  return answer;
+
+  return result.size;
 }
+
+// 다른 풀이
+// function solution(elements) {
+//   const circular = elements.concat(elements);
+//   const set = new Set();
+
+//   for (let i = 0; i < elements.length; i++) {
+//     let sum = 0;
+//     for (let j = 0; j < elements.length; j++) {
+//       sum += circular[i + j];
+//       set.add(sum);
+//     }
+//   }
+
+//   return set.size;
+// }
