fb963ca66d611ffc6113a6f058b63303d0bdd76b
jeong <chan3358@naver.com>
2023-04-20 17:42:39 +0900

    refactor: simplify 17687

--- programmers/js/level02/17687.js
+++ programmers/js/level02/17687.js
@@ -1,15 +1,15 @@
 // [3차] n진수 게임
 function solution(n, t, m, p) {
-  var answer = '';
-  let result = '';
+  let answer = "";
+  let result = "";
 
-  for (let i = 0; i < t * m; i++) {
-    result += i.toString(n).toUpperCase();
+  for (let i = 0; result.length < t * m; i++) {
+    result += i.toString(n);
   }
 
   for (let i = 0; i < t; i++) {
     answer += result[p - 1 + m * i];
   }
 
-  return answer;
+  return n > 10 ? answer.toUpperCase() : answer;
 }
