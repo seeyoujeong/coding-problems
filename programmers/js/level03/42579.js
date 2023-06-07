d4bbde9ed51132f04fcd918c73a94c802b73c8f9
jeong <chan3358@naver.com>
2023-06-07 14:50:52 +0900

    feat: add 42579 to level03

---
+++ programmers/js/level03/42579.js
@@ -0,0 +1,42 @@
+// 베스트앨범
+function solution(genres, plays) {
+  const genreMap = new Map();
+
+  for (let i = 0; i < genres.length; i += 1) {
+    const data = genreMap.get(genres[i]) || { total: 0, songs: [] };
+
+    genreMap.set(genres[i], {
+      total: data.total + plays[i],
+      songs: [...data.songs, { play: plays[i], index: i }]
+        .sort((a, b) => b.play - a.play)
+        .slice(0, 2),
+    });
+  }
+
+  return [...genreMap]
+    .sort((a, b) => b[1].total - a[1].total)
+    .flatMap((item) => item[1].songs)
+    .map((song) => song.index);
+}
+
+// 데브코스
+// function solution(genres, plays) {
+//   const genreMap = new Map();
+
+//   genres
+//     .map((genre, index) => [genre, plays[index]])
+//     .forEach(([genre, play], index) => {
+//       const data = genreMap.get(genre) || { total: 0, songs: [] };
+//       genreMap.set(genre, {
+//         total: data.total + play,
+//         songs: [...data.songs, { play, index }]
+//           .sort((a, b) => b.play - a.play)
+//           .slice(0, 2),
+//       });
+//     });
+
+//   return [...genreMap.entries()]
+//     .sort((a, b) => b[1].total - a[1].total)
+//     .flatMap((item) => item[1].songs)
+//     .map((song) => song.index);
+// }
