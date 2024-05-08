e0792137b6e96d5941d4c34c7bbdfaf6e05034c2
seeyoujeong <chan3358@naver.com>
2024-05-08 10:10:49 +0900

    refactor: 풀이 수정

--- programmers/js/basic/181829.js
+++ programmers/js/basic/181829.js
@@ -1,12 +1,7 @@
 // 이차원 배열 대각선 순회하기
 function solution(board, k) {
-  return board.reduce((acc, cur, i) => {
-    cur.forEach((num, j) => {
-      if (i + j <= k) {
-        acc += num;
-      }
-    });
-
-    return acc;
-  }, 0);
+  return board
+    .filter((_, i) => i <= k)
+    .flatMap((arr, i) => arr.filter((_, j) => i + j <= k))
+    .reduce((acc, cur) => acc + cur, 0);
 }
