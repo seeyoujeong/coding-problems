a17744082e0a06887f0c2b242a058ec0fb66a695
seeyoujeong <chan3358@naver.com>
2024-05-22 10:10:34 +0900

    fix: 풀이 수정

--- programmers/js/level00/120869.js
+++ programmers/js/level00/120869.js
@@ -1,6 +1,9 @@
 // 외계어 사전
+
 function solution(spell, dic) {
-  return dic
-    .filter(d => spell.every(s => d.includes(s)))
-    .length > 0 ? 1 : 2;
+  return dic.filter((d) =>
+    spell.every((s) => d.includes(s) && d.indexOf(s) === d.lastIndexOf(s))
+  ).length > 0
+    ? 1
+    : 2;
 }
