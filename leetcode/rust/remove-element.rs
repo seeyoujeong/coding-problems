bf6e1c37a5117df5517543a242fcf39b90731703
seeyoujeong <chan3358@naver.com>
2024-08-21 20:25:13 +0900

    feat: rust 풀이 추가

---
+++ leetcode/rust/remove-element.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut count = 0;
+
+        for i in 0..nums.len() {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count += 1;
+            }
+        }
+
+        count as i32
+    }
+}
