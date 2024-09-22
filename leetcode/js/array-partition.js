a6ba82a9411e5c70c1b550cb35e10a31699ed515
seeyoujeong <seeyoujeong.dev@gmail.com>
2024-09-22 20:59:01 +0900

    feat: js 풀이 추가

---
+++ leetcode/js/array-partition.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i += 2) {
+    result += Math.min(nums[i], nums[i + 1]);
+  }
+
+  return result;
+};
