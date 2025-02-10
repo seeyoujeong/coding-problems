6ccfaea7f898a411c009bb2a9a8afaca8807b9d5
seeyoujeong <seeyoujeong.dev@gmail.com>
2025-02-10 19:18:50 +0900

    feat: rust 풀이 추가

---
+++ baekjoon/rust/10813.rs
@@ -0,0 +1,24 @@
+use std::io::{stdin, stdout, BufWriter, Write};
+
+fn main() {
+    let mut lines = stdin().lines();
+    let line = lines.next().unwrap().unwrap();
+    let mut line = line.split_whitespace().flat_map(str::parse::<u16>);
+
+    let n = line.next().unwrap();
+    let mut result: Vec<u16> = (1..=n).collect();
+
+    while let Some(Ok(line)) = lines.next() {
+        let mut nums = line.split_whitespace().flat_map(str::parse::<usize>);
+        let i = nums.next().unwrap() - 1;
+        let j = nums.next().unwrap() - 1;
+
+        result.swap(i, j);
+    }
+
+    let mut output = BufWriter::new(stdout());
+
+    for num in result {
+        write!(output, "{} ", num).unwrap();
+    }
+}
