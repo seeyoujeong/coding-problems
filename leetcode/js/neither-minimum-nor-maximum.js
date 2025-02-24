38828bc09e9a75044d98dbe8a05e8df2bbe29252
seeyoujeong <seeyoujeong.dev@gmail.com>
2025-02-24 11:21:14 +0900

    feat: js 풀이 추가

---
+++ leetcode/js/neither-minimum-nor-maximum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function (nums) {
+  if (nums.length <= 2) {
+    return -1;
+  }
+
+  nums.sort((a, b) => a - b);
+
+  return nums[1];
+};
