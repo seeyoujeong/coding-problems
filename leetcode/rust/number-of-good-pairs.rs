abf41f866c4f4688c947cbbf39da91defee66ff4
seeyoujeong <seeyoujeong.dev@gmail.com>
2025-07-23 20:33:28 +0900

    feat: 기존 풀이 수정

--- leetcode/rust/number-of-good-pairs.rs
+++ leetcode/rust/number-of-good-pairs.rs
@@ -1,8 +1,8 @@
-impl Solution {
+impl Solution1 {
     pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
         let mut result = 0;
 
-        for i in 0..nums.len() {
+        for i in 0..(nums.len() - 1) {
             for j in (i + 1)..nums.len() {
                 if nums[i] == nums[j] {
                     result += 1;
@@ -11,5 +11,22 @@ impl Solution {
         }
 
         result
+    }
+}
+
+impl Solution2 {
+    pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
+
+        let mut map = HashMap::new();
+        let mut count = 0;
+
+        for num in nums {
+            let val = map.entry(num).or_insert(0);
+            count += *val;
+            *val += 1
+        }
+
+        count
     }
 }
