7a744fa9b98c5487332a644d7895e45a3fe35f5a
seeyoujeong <seeyoujeong.dev@gmail.com>
2026-03-26 19:55:28 +0900

    feat: python 풀이 추가

---
+++ leetcode/python/decompress-run-length-encoded-list.py
@@ -0,0 +1,10 @@
+class Solution:
+    def decompressRlElist(self, nums: list[int]) -> list[int]:
+        result = []
+
+        for i in range(0, len(nums), 2):
+            freq = nums[i]
+            val = nums[i+1]
+            result.extend([val] * freq)
+
+        return result
