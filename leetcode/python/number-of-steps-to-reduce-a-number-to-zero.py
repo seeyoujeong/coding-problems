857c6822266f8868ecd135f7e89cad16cad7d4dc
seeyoujeong <seeyoujeong.dev@gmail.com>
2026-04-01 20:34:26 +0900

    feat: python 풀이 추가

---
+++ leetcode/python/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        result = 0
+
+        while num > 0:
+            num = num / 2 if num % 2 == 0 else num - 1
+            result += 1
+
+        return result
