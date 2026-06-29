c73a7cefd818d7824903f62f1ba6585138e8e200
seeyoujeong <seeyoujeong.dev@gmail.com>
2026-06-29 17:18:59 +0900

    feat: python 풀이 추가

---
+++ leetcode/python/clear-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+
+        for c in s:
+            if c.isdigit():
+                stack.pop()
+            else:
+                stack.append(c)
+        
+        return "".join(stack)
